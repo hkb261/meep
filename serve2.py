302684b08272f3432b692bdc2e7fb79d9dd3e33a
Hayden Boroski <hkb261@gmail.com>
2012-03-13 15:40:48 -0400

    Forgot to include the ip variable instead of google's hardcoded ip variable for testing in the 'SERVER_NAME' variable

--- serve2.py
+++ serve2.py
@@ -63,7 +63,7 @@ def process_incoming(data,ip,port):
 	e['PATH_INFO'] = '/'
 	e['QUERY_STRING'] = ''
 	e['SERVER_PROTOCOL'] = 'HTTP/1.1'
-	e['SERVER_NAME'] = socket.gethostbyaddr("69.59.196.211")
+	e['SERVER_NAME'] = socket.gethostbyaddr(ip)
 	e['SERVER_PORT'] = str(port)
 	e['CONTENT_TYPE'] = 'text/plain'
 	e['CONTENT_LENGTH'] = '0'
